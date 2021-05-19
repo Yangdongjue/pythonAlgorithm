f05cc666df1096daf384725596a723ec19e1fcee
yangdongjue <djwhy5510@naver.com>
2021-05-20 06:13:09 +0900

    2-7

--- ch2/2-6.py
+++ ch2/2-6.py
@@ -1,24 +1,27 @@
 import sys
-
-N= int(input())
-arr=list(map(int, input().split()))
-sumArr=[]
-for i in arr:
+def digit_sum(x):
     n=1
     sum=0
     while True:
-        if i//n==0:
+        if x//n==0:
             n/=10
             break
         else: n*=10
     
     while True:
-        k=i//n
-        i=i-k*n
+        k=x//n
+        x=x-k*n
         sum+=k
         if(n==1):
             break
         else: n/=10
+    return sum
+    
+N= int(input())
+arr=list(map(int, input().split()))
+sumArr=[]
+for i in arr:
+    sum=digit_sum(i)
     sumArr.append(sum)
 max=0
 for i in range(len(sumArr)):
---
+++ ch2/2-7.py
@@ -0,0 +1,28 @@
+import sys
+
+N= int(input())
+m=[2]
+for i in range(3, N+1):
+    for j in m:
+        if i%j==0:
+            break;
+        if  j==m[len(m)-1] and i%j!=0:
+            m.append(i)
+            break;
+
+print(len(m))
+
+cnt=0
+#배열 선언
+#k=[]
+#for i in range(N+1):
+#    k.append(0)
+k=[0]*(n+1)
+
+for i in range(2,N+1):
+    if k[i]==0:
+        cnt+=1
+        for j in range(1,N//i+1): #for j in range(i, n+1, i):
+            k[j*i]=1
+
+print(cnt)
